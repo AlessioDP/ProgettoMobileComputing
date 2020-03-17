692e5b2c066ba87b9d123c66a3f5848ae42a7cf6
AlessioDP <alessiodptv@gmail.com>
2020-03-17 23:45:15 +0100

    Aggiungere i file di progetto.

---
+++ ProgettoMobileComputing/ProgettoMobileComputing/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ProgettoMobileComputing.Models;
+
+namespace ProgettoMobileComputing.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
