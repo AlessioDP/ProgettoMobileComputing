87199d1df4a88f7d4e6a6c42f085af04d09db033
wodo96 <61684402+wodo96@users.noreply.github.com>
2020-04-13 12:48:24 +0200

    Add Position Detail on Item

--- ProgettoMobileComputing/ProgettoMobileComputing/Models/Item.cs
+++ ProgettoMobileComputing/ProgettoMobileComputing/Models/Item.cs
@@ -2,10 +2,38 @@
 
 namespace ProgettoMobileComputing.Models
 {
+
+    public class Group
+    {
+        public string Name { get; set; }
+
+    }
+
+    public class Position
+    {
+        public string Home { get; set; }
+        public string Room { get; set; }
+        public Group Group { get; set; }
+
+        public string Text
+        {
+            get
+            {
+                return this.Home + " > " + this.Room + " > " + Group.Name;
+            }
+        }
+
+    }
+
+    
+
+
     public class Item
     {
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
+        public Position Position { get; set; }
+
     }
 }
